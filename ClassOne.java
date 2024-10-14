233b6972f4c7eb5d9a2dcc44045e6524f94ba853
habibur4009 <mdhabibur4009@gmail.com>
2024-10-14 15:50:25 +0600

    Added Scanner Clas and Get User Name

--- ClassOne.java
+++ ClassOne.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class ClassOne {
 
     public static void main(String[] args) {
-        
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter Name");
+        String name=scanner.nextLine();
     }
 }
