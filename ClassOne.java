4e09a5e88c75393e244a7b580fe576daad0f500f
habibur4009 <mdhabibur4009@gmail.com>
2024-10-14 16:03:02 +0600

    Remove the Scanner Class and it's funtionality

--- ClassOne.java
+++ ClassOne.java
@@ -1,10 +1,8 @@
-import java.util.Scanner;
+
 
 public class ClassOne {
 
     public static void main(String[] args) {
-        Scanner scanner =new Scanner(System.in);
-        System.out.println("Enter Name");
-        String name=scanner.nextLine();
+       //Do again the merge
     }
 }
